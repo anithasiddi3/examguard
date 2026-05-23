c3f7948edf8382eaaa66ae8ff98372e380734231
anithasiddi3 <anithasiddi3@gmail.com>
2026-05-23 08:23:44 +0530

    add vercel routing fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,67 +1,15 @@
-import React from "react";
-import {
-BrowserRouter,
-Routes,
-Route
-} from "react-router-dom";
-
-import LoginPage from "./pages/LoginPage";
-import ExamPage from "./pages/ExamPage";
-import AnalyticsDashboard from "./pages/AnalyticsDashboard";
-import ProctorDashboard from "./pages/ProctorDashboard";
-import ResultPage from "./pages/ResultPage";
-import Leaderboard from "./pages/Leaderboard";
-import CreateExamPage from "./pages/CreateExamPage";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
-return (
-
-<BrowserRouter>
-
-<Routes>
-
-<Route
-path="/"
-element={<LoginPage/>}
-/>
-
-<Route
-path="/exam"
-element={<ExamPage/>}
-/>
-
-<Route
-path="/analytics"
-element={<AnalyticsDashboard/>}
-/>
-
-<Route
-path="/create-exam"
-element={<CreateExamPage/>}
-/>
-
-<Route
-path="/proctor"
-element={<ProctorDashboard/>}
-/>
-
-<Route
-path="/results"
-element={<ResultPage/>}
-/>
-
-<Route
-path="/leaderboard"
-element={<Leaderboard/>}
-/>
-
-</Routes>
-
-</BrowserRouter>
-
-);
-
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<ExamList />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/results" element={<ResultPage />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
