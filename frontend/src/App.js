72e78c697a26b755ecef02226440af621a1ab66c
anithasiddi3 <anithasiddi3@gmail.com>
2026-05-26 09:10:29 +0530

    fix app routing and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,44 @@
 import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
+import "./styles/App.css";
+
+import HomePage from "./components/HomePage";
 import ExamPage from "./pages/ExamPage";
+import Leaderboard from "./components/Leaderboard";
+import ResultPage from "./components/ResultPage";
+import AdminPage from "./components/AdminPage";
+import ProctorDashboard from "./components/ProctorDashboard";
+import AnalyticsPage from "./components/AnalyticsPage";
 
 function App() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<ExamPage />} />
-      </Routes>
-    </HashRouter>
-  );
+
+return (
+
+<HashRouter>
+
+<Routes>
+
+<Route path="/" element={<HomePage />} />
+
+<Route path="/exam" element={<ExamPage />} />
+
+<Route path="/leaderboard" element={<Leaderboard />} />
+
+<Route path="/results" element={<ResultPage />} />
+
+<Route path="/admin" element={<AdminPage />} />
+
+<Route path="/proctor" element={<ProctorDashboard />} />
+
+<Route path="/analytics" element={<AnalyticsPage />} />
+
+</Routes>
+
+</HashRouter>
+
+);
+
 }
 
 export default App;
